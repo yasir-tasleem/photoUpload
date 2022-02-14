262daffcad09bb3e7f35f66cb437ffad3a39ab29
mohammad.yasir <yasirmohammed782@gmail.com>
2022-02-14 15:50:44 +0530

    initial commit

---
+++ src/main/java/com/pillarglobal/photoUpload/PhotoUploadApplication.java
@@ -0,0 +1,13 @@
+package com.pillarglobal.photoUpload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pillarglobal/photoUpload/PhotoUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pillarglobal.photoUpload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

9dfc57e5c0f5d9a1f7f230f5ce49eaa8336835f8
mohammad.yasir <yasirmohammed782@gmail.com>
2022-02-17 01:21:36 +0530

    added validations and guid

--- src/main/java/com/pillarglobal/photoUpload/controllers/photoUploadController.java
+++ src/main/java/com/pillarglobal/photoUpload/controllers/photoUploadController.java
@@ -3,7 +3,6 @@ package com.pillarglobal.photoUpload.controllers;
 import com.pillarglobal.photoUpload.service.PhotoUploadService;
 import com.pillarglobal.photoUpload.service.S3BucketStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/pillarglobal/photoUpload/service/S3BucketStorageService.java
+++ src/main/java/com/pillarglobal/photoUpload/service/S3BucketStorageService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 public class S3BucketStorageService {
@@ -24,20 +25,27 @@ public class S3BucketStorageService {
     @Value("${application.bucket.name}")
     private String bucketName;
 
-    /**
-     * Upload file into AWS S3
-     *
-     * @param keyName
-     * @param file
-     * @return String
-     */
-    public String uploadFile(String keyName, MultipartFile file) {
+    private UUID photoGuid;
+
+    public String uploadFile(String keyName, MultipartFile file)  {
         try {
             ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(file.getSize());
-            amazonS3Client.putObject(bucketName, keyName, file.getInputStream(), metadata);
-            return "File uploaded: " + keyName;
-        } catch (IOException ioe) {
+            logger.info("file.getSize() :" + file.getContentType() );
+            logger.info("file.getContentType() :" + file.getContentType());
+            if(!file.isEmpty() && (file.getContentType().equals("image/jpeg"))
+                    || (file.getContentType().equals("image/png"))
+                    || (file.getContentType().equals("image/jpg"))
+                    && file.getSize() <= 20000){
+
+                photoGuid = java.util.UUID.randomUUID();
+                logger.info("photoGuid : " + photoGuid);
+                metadata.setContentLength(file.getSize());
+                amazonS3Client.putObject(bucketName, String.valueOf(photoGuid), file.getInputStream(), metadata);
+                logger.info(String.valueOf(amazonS3Client.getUrl("bucket","hi")));
+                return "File uploaded: " + photoGuid;
+            }
+            else throw new Exception("Image not accepted");
+           } catch (IOException ioe) {
             logger.error("IOException: " + ioe.getMessage());
         } catch (AmazonServiceException serviceException) {
             logger.info("AmazonServiceException: " + serviceException.getMessage());
@@ -45,6 +53,9 @@ public class S3BucketStorageService {
         } catch (AmazonClientException clientException) {
             logger.info("AmazonClientException Message: " + clientException.getMessage());
             throw clientException;
+        } catch (Exception e) {
+            logger.error("IOException: " + e.getMessage());
+            return "File not uploaded: " + e.getMessage();
         }
         return "File not uploaded: " + keyName;
     }
